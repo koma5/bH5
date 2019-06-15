ef360b3bc9743151365934af417bfb0eb8b3dd39
Marco Koch <marco@5th.ch>
2019-06-16 00:05:33 +0100

    track index; track detail; track_by_user index

--- tracks/urls.py
+++ tracks/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('track/<int:track_id>', views.detail, name='detail'),
+    path('user/<username>/', views.index_by_user, name='index_by_user'),
 ]
--- tracks/views.py
+++ tracks/views.py
@@ -1,11 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 from .models import Track
 from .models import Point
 
 def index(request):
-    track_count = Track.objects.count()
-    point_count = Point.objects.count()
-    return HttpResponse("bH5 is tracking you. There {1} currently {0} point{2} in the database.".format(point_count, 'is' if point_count == 1 else 'are', '' if point_count == 1 else 's'))
 
+    context = {
+	'tracks': Track.objects.order_by('-id')[:50],
+        'point_count': Track.objects.count(),
+        'track_count': Point.objects.count(),
+    }
+
+    template = loader.get_template('tracks/index.html')
+
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, track_id):
+
+    track =  Track.objects.get(id=track_id)
+
+    context = {
+        'track':  track,
+        #'points2': mytrack.points,
+        'points': Point.objects.filter(track=track).order_by('-id')[:50],
+    }
+
+    return HttpResponse(render(request, 'tracks/detail.html', context))
+
+def index_by_user(request, username):
+
+    context = {
+        'tracks': Track.objects.filter(owner__username=username).order_by('-id')[:50],
+    }
+
+    return HttpResponse(render(request, 'tracks/index.html', context))
