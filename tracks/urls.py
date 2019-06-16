29739045a88bc5e6aec65599c279892a5c824903
Marco Koch <marco@5th.ch>
2019-06-16 06:05:51 +0100

    tracks url naming

--- tracks/urls.py
+++ tracks/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'tracks'
 urlpatterns = [
     path('', views.index, name='index'),
     path('track/<int:track_id>', views.detail, name='detail'),
--- tracks/views.py
+++ tracks/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-from django.http import Http404
 
 from .models import Track
 from .models import Point
