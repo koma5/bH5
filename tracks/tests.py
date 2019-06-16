a25b3d28c7e608fc4790ddcc172800643c221c44
Marco Koch <marco@5th.ch>
2019-06-16 22:52:40 +0100

    first test: hide non-public tracks

--- tracks/tests.py
+++ tracks/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from .models import Track
+
+class ViewFunctionTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_dont_show_private_tracks_on_index_if_unauthenticated(self):
+
+        name = 'testSecretTrack'
+
+        user = User()
+        user.save()
+
+        track = Track(name=name, public=False, owner=user)
+        track.save()
+
+        url = reverse('tracks:index')
+        response = self.client.get(url)
+
+        #print(response.content)
+
+        self.assertNotContains(response, name)
+
