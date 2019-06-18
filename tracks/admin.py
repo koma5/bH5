fa25f235faa7805997d6a9d0ff67d707f4221407
Marco Koch <marco@5th.ch>
2019-06-18 12:46:32 +0100

    Segment inline of Track in admin backend.

--- tracks/admin.py
+++ tracks/admin.py
@@ -3,6 +3,15 @@ from django.contrib import admin
 
 from .models import Point, Segment, Track
 
+class SegmentInLine(admin.StackedInline):
+    model = Segment
+    extra = 0
+
+class TrackAdmin(admin.ModelAdmin):
+    inlines = [SegmentInLine]
+
+
 admin.site.register(Point)
 admin.site.register(Segment)
-admin.site.register(Track)
+admin.site.register(Track, TrackAdmin)
+
